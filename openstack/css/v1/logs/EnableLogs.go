b740e286f424a2849040e6143330b519fc2947ef
sattila1999 <somogyiattila417@gmail.com>
2025-10-20 14:04:39 +0200

    Css logging fixes

--- openstack/css/v1/logs/EnableLogs.go
+++ openstack/css/v1/logs/EnableLogs.go
@@ -5,24 +5,46 @@ import (
 	"github.com/opentelekomcloud/gophertelekomcloud/internal/build"
 )
 
-type EnableLogsOpts struct {
+type LogsOpts struct {
 	// These parameters are passed to the logs.EnableLogs function.
 	// Agency is the agency name used for the css cluster.
-	Agency string `json:"agency" required:"true"`
+	Agency string `json:"agency"`
 	// BasePath is the obs path where the logs should be stored for the css cluster.
-	BasePath string `json:"logBasePath" required:"true"`
+	BasePath string `json:"logBasePath"`
 	// Bucket is the obs bucket name to store the logs for the css cluster.
-	Bucket string `json:"logBucket" required:"true"`
+	Bucket string `json:"logBucket"`
+	// Index prefix for saving logs.
+	IndexPrefix string `json:"index_prefix"`
+	// Log retention duration.
+	KeepDays int `json:"keep_days"`
+	// Specifies the target cluster for saving logs.
+	TargetClusterId string `json:"target_cluster_id"`
 }
 
-// EnableLogs function is used to enable the log switch of a CSS cluster base on EnableLogsOpts.
-func EnableLogs(client *golangsdk.ServiceClient, clusterID string, opts EnableLogsOpts) error {
-	b, err := build.RequestBody(opts, "")
+// EnableBaseLogs will enable log switch for the CSS cluster.
+func EnableBaseLogs(client *golangsdk.ServiceClient, clusterID string, logSwitchOpts *LogsOpts) error {
+	action := "base_log_collect"
+	return enableLogs(client, clusterID, logSwitchOpts, &action)
+}
+
+// EnableRealTimeLogs will enable log ingestion for the CSS cluster.
+func EnableRealTimeLogs(client *golangsdk.ServiceClient, clusterID string, logSwitchOpts *LogsOpts) error {
+	action := "real_time_log_collect"
+	return enableLogs(client, clusterID, logSwitchOpts, &action)
+}
+
+// EnableLogs function is used to enable the "Log Backup" or the "Log Ingestion" switch of a CSS cluster based on EnableLogsOpts.
+func enableLogs(client *golangsdk.ServiceClient, clusterID string, logSwitchOpts *LogsOpts, action *string) error {
+	b, err := build.RequestBody(*logSwitchOpts, "")
 	if err != nil {
 		return err
 	}
 
 	url := client.ServiceURL("clusters", clusterID, "logs", "open")
+
+	if action != nil && *action != "" {
+		url += "?action=" + *action
+	}
 
 	_, err = client.Post(url, b, nil, &golangsdk.RequestOpts{
 		OkCodes: []int{200},
